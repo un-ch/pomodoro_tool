1b1183dea3e1bcce9337e20f5fd3cc15e1a74a11
Anatolii Charikov <pepsh6881@gmail.com>
2024-07-18 12:31:56 +0300

    Updated.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- pomodoro_tool.sh
+++ pomodoro_tool.sh
@@ -9,10 +9,15 @@ else
 fi
 
 SECONDS_SHOWING_NOTIFYING_BADGE=30
-msg='########## TIME! ##########'
+msg='############## _____ TIME! _____ ##############'
 
 let SECONDS_POMODORO_INTERVAL=$MINUTES_POMODORO_INTERVAL*60
 let MILISECONDS_SHOWING_NOTIFYING_BADGE=$SECONDS_SHOWING_NOTIFYING_BADGE*1000
 
+# notify-send -t $MILISECONDS_SHOWING_NOTIFYING_BADGE "$msg"
+
 sleep $SECONDS_POMODORO_INTERVAL &&
-notify-send -t $MILISECONDS_SHOWING_NOTIFYING_BADGE "$msg"
+notify-send \
+    -t $MILISECONDS_SHOWING_NOTIFYING_BADGE \
+    '############## _____ TIME! _____ ##############' \
+    'This is an example notification.' --icon=dialog-information
