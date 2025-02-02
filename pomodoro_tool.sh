1ad8f6eec039e955507c5b5345efe57417a91702
Anatolii Charikov <pepsh6881@gmail.com>
2025-02-02 12:23:09 +0200

    Launching timer program was removed.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- pomodoro_tool.sh
+++ pomodoro_tool.sh
@@ -5,21 +5,19 @@ FIRST_COMMAND_LINE_ARGUMENT="$1"
 if [ "$#" -ne 0 ]; then	
 	MINUTES_POMODORO_INTERVAL=$FIRST_COMMAND_LINE_ARGUMENT
 else
-	MINUTES_POMODORO_INTERVAL=1
+	MINUTES_POMODORO_INTERVAL=30
 fi
 
-SECONDS_SHOWING_NOTIFYING_BADGE=30
+SECONDS_SHOWING_NOTIFYING_BADGE=60
 msg='############## _____ TIME! _____ ##############'
 
-let SECONDS_POMODORO_INTERVAL=$MINUTES_POMODORO_INTERVAL*2
+let SECONDS_POMODORO_INTERVAL=$MINUTES_POMODORO_INTERVAL*60
 let MILISECONDS_SHOWING_NOTIFYING_BADGE=$SECONDS_SHOWING_NOTIFYING_BADGE*1000
 
 # notify-send -t $MILISECONDS_SHOWING_NOTIFYING_BADGE "$msg"
 
-sleep $SECONDS_POMODORO_INTERVAL && exec /home/us/prj/timer/build/timer
-
-# exec /home/us/prj/foowm/build/foowm
-#notify-send \
-#    -t $MILISECONDS_SHOWING_NOTIFYING_BADGE \
-#    '############## _____ TIME! _____ ##############' \
-#    'This is an example notification.' --icon=dialog-information
+sleep $SECONDS_POMODORO_INTERVAL &&
+notify-send \
+    -t $MILISECONDS_SHOWING_NOTIFYING_BADGE \
+    '############## _____ TIME! _____ ##############' \
+    'This is an example notification.' --icon=dialog-information
