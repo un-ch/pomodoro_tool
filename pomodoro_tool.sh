bf0e308bad222465af4b0177f9fe7672a4bcc894
Anatolii Charikov <acharikov@proton.me>
2025-05-27 16:16:32 +0300

    Add notification options.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- pomodoro_tool.sh
+++ pomodoro_tool.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 DEFAULT_MINUTES=30
-NOTIFY_DURATION_SECONDS=60
+NOTIFY_DURATION_SECONDS=30
 NOTIFY_TITLE='############## ----- TIME! ----- ##############'
-NOTIFY_BODY='Take a break.'
 NOTIFY_ICON='dialog-information'
 
-# --- argument parsing ---
 if [[ $# -eq 0 ]]; then
 	MINUTES=$DEFAULT_MINUTES
 elif [[ "$1" =~ ^[0-9]+$ ]]; then
@@ -16,17 +14,25 @@ else
 	exit 1
 fi
 
-# --- calculations ---
 SECONDS_TO_WAIT=$((MINUTES * 60))
 NOTIFY_DURATION_MS=$((NOTIFY_DURATION_SECONDS * 1000))
 
-# --- countdown ---
 echo "Pomodoro started: $MINUTES minutes."
 sleep "$SECONDS_TO_WAIT"
 
-# --- notification ---
-notify-send \
-	-t "$NOTIFY_DURATION_MS" \
-	"$NOTIFY_TITLE" \
-	"$NOTIFY_BODY" \
+if [[ -n $(command -v notify-send) ]]; then
+	notify-send \
+	-t "$NOTIFY_DURATION_MS" "$NOTIFY_TITLE" \
 	--icon="$NOTIFY_ICON"
+
+elif [[ -n $(command -v xmessage) ]]; then
+	xmessage -center -buttons "" -geometry 300x100 -bg "#282c34" -fg "#abb2bf" \
+		"$NOTIFY_TITLE" &
+
+	XMESSAGE_PID=$!
+
+	sleep "$NOTIFY_DURATION_SECONDS"
+	kill "$XMESSAGE_PID" 2>/dev/null
+else
+	echo "$NOTIFY_TITLE"
+fi
